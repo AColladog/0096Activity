577b3cb0c32893a4e943c82fc7ba77032b849689
Angel <ese.acg@gmail.com>
2014-03-31 21:04:04 +0200

    método generateResponse, hecho ahora con Iterador, pero misma solución que el commit anterior

--- Responder.java
+++ Responder.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Iterator;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -11,19 +12,39 @@ import java.util.Random;
  */
 public class Responder
 {   
-    private HashMap<String, String> hm;
+    private HashMap<HashSet<String>, String> hm;
     private Random aleatorio;
     private ArrayList<String> respuestas;
+    private HashSet<String> hs10;
+    private HashSet<String> hs11;
+    private HashSet<String> hs12;
+    private HashSet<String> hs20;
+    private HashSet<String> hs21;
+    private HashSet<String> hs22;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        hs10 = new HashSet<>();
+        hs10.add("averia"); 
+
+        hs11 = new HashSet<>();
+        hs11.add("conexion");
+  
+
+        hs20 = new HashSet<>();
+        hs20.add("error");
+
+        hs21 = new HashSet<>();
+        hs21.add("factura");
+
+
         hm = new HashMap <>();
-        hm.put("averia","Nos pondremos a trabajar en su averia");
-        hm.put("conexion", "Arreglaremos esa conexion");
-        hm.put("error", "Reinicie el equipo, para ver si remite el error");
-        hm.put("factura", "Revisaremos su factura");
+        hm.put(hs10,"Nos pondremos a trabajar en su averia");
+        hm.put(hs11, "Arreglaremos esa conexion");
+        hm.put(hs20, "Reinicie el equipo, para ver si remite el error");
+        hm.put(hs21, "Revisaremos su factura");
 
         aleatorio = new Random();
         respuestas = new ArrayList<>();
@@ -41,18 +62,20 @@ public class Responder
      */
     public HashSet<String> generateResponse(HashSet<String> entrada){
         HashSet<String> respuestaDevuelta = new HashSet<>(); 
-        
-        for(String key: hm.keySet()){
-            if(entrada.contains(key)){
-                respuestaDevuelta.add(hm.get(key));
-            }
+
+        for(HashSet<String> key: hm.keySet()){            
+            Iterator<String> it = key.iterator();            
+            while(it.hasNext()){
+                if(entrada.contains(it.next())){
+                    respuestaDevuelta.add(hm.get(key));
+                }                
+            }            
         }
         if(respuestaDevuelta.isEmpty()){
-                respuestaDevuelta.add( respuestas.get( aleatorio.nextInt(respuestas.size()) ) );
-            }
+            respuestaDevuelta.add( respuestas.get( aleatorio.nextInt(respuestas.size()) ) );
+        }
         return respuestaDevuelta;
     }
-    
 
     /** String respuesta = "";        
      *  if(hm.containsKey(entrada) ){
